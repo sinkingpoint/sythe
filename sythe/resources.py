80df85ca23d1fb2098d99ac097d54ee27c5f0dae
sinkingpoint <colin@quirl.co.nz>
2017-04-04 08:21:22 +1200

    Fixed test errors for get_without_pages

--- sythe/resources.py
+++ sythe/resources.py
@@ -7,10 +7,22 @@ class Resource:
     def __getitem__(self, key):
         return self.data[key]
 
+    def __eq__(self, A):
+        if isinstance(A, Resource):
+            return A.data == self.data
+        return False
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return repr(self.data)
+
 class EC2Instance(Resource):
     def __init__(self, data):
-        for tag in data['Tags']:
-            data['tag:{}'.format(tag['Key'])] = tag['Value']
+        if 'Tags' in data:
+            for tag in data['Tags']:
+                data['tag:{}'.format(tag['Key'])] = tag['Value']
         Resource.__init__(self, data)
 
 def get_ec2_instances(ec2_client=boto3.client('ec2')):
