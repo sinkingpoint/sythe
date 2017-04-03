1e352af194f9272ad0877075428c4c0254dab7b4
Colin Douch <colin@quirl.co.nz>
2017-04-03 21:27:06 +1200

    Moved EC2Instance logic into class

--- sythe/resources.py
+++ sythe/resources.py
@@ -1,5 +1,18 @@
 import boto3
 
+class Resource:
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+class EC2Instance(Resource):
+    def __init__(self, data):
+        for tag in data['Tags']:
+            data['tag:{}'.format(tag['Key'])] = tag['Value']
+        Resource.__init__(self, data)
+
 def get_ec2_instances(ec2_client=boto3.client('ec2')):
     instances = []
     instance_page = ec2_client.describe_instances()
@@ -11,12 +24,7 @@ def get_ec2_instances(ec2_client=boto3.client('ec2')):
         instance_from_page = [instance for reservation in instance_page['Reservations']
                               for instance in reservation['Instances']]
         instances = instances + instance_from_page
-    return [augment_ec2_instance_resource(instance) for instance in instances]
-
-def augment_ec2_instance_resource(resource):
-    for tag in resource['Tags']:
-        resource['tag:{}'.format(tag['Key'])] = tag['Value']
-    return resource
+    return [EC2Instance(instance) for instance in instances]
 
 def filter_resources(resources, condition):
     return [resource for resource in resources if condition.execute(resource)]
