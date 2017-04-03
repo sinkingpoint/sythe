f5fa79ef998c0ed9be400cf0a32673a2edebd87e
sinkingpoint <colin@quirl.co.nz>
2017-04-03 17:33:55 +1200

    Started method to parse rules files

--- sythe/cli.py
+++ sythe/cli.py
@@ -1,7 +1,10 @@
 import argparse
+import fileio
 
 def main():
     parser = argparse.ArgumentParser(description='A rule engine for resources')
     parser.add_argument('config', help='The config file containing rules')
     args = parser.parse_args()
 
+    config_file_path = args.config
+    rules = fileio.parse_rules_from_file(config_file_path)
---
+++ sythe/fileio.py
@@ -0,0 +1,17 @@
+"""
+This module provides IO operations for moving rules between text
+representations and internal AST representations
+"""
+
+import sythe.parsing.tokenizer as tokenizer
+
+def parse_rules_from_file(file_path):
+    """
+    This function reads the given file and parses
+    all the rule definitions, throwing a ParsingError
+    if they are incorrect
+    """
+    with open(file_path, 'r') as rule_file:
+        raw_file_contents = rule_file.read()
+
+    return []
