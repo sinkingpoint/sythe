53afa36740d46fd5e8edfa25c736f2d6b38e95fd
Colin Douch <colin@quirl.co.nz>
2017-04-03 21:21:20 +1200

    Added method to augment ec2 instance blobs

--- sythe/resources.py
+++ sythe/resources.py
@@ -6,12 +6,17 @@ def get_ec2_instances(ec2_client=boto3.client('ec2')):
     instance_from_page = [instance for reservation in instance_page['Reservations']
                           for instance in reservation['Instances']]
     instances = instances + instance_from_page
-    while instance_page['NextToken']:
+    while 'NextToken' in instance_page and instance_page['NextToken']:
         instance_page = ec2_client.describe_instances(NextToken=instance_page['NextToken'])
         instance_from_page = [instance for reservation in instance_page['Reservations']
                               for instance in reservation['Instances']]
         instances = instances + instance_from_page
-    return instances
+    return [augment_ec2_instance_resource(instance) for instance in instances]
+
+def augment_ec2_instance_resource(resource):
+    for tag in resource['Tags']:
+        resource['tag:{}'.format(tag['Key'])] = tag['Value']
+    return resource
 
 def filter_resources(resources, condition):
     return [resource for resource in resources if condition.execute(resource)]
