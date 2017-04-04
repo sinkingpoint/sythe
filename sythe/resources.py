204e9a4ee5cc642467a1ec76402510fef95d3006
sinkingpoint <colin@quirl.co.nz>
2017-04-05 11:15:34 +1200

    Added docstrings to resources

--- sythe/resources.py
+++ sythe/resources.py
@@ -1,11 +1,26 @@
+"""
+This module defines resources which can be used in sythe.
+Resources are objects that contain metadata and define a number of actions
+that can be defined across that resource
+"""
+
+from datetime import datetime
+import parsedatetime
 import sythe.aws
 import sythe.errors as errors
-import parsedatetime
-from datetime import datetime
 
 def resource_action(required_args):
+    """
+    A decorator which enforces that the args dict passed to an
+    action contains the required arguments
+    """
     def enforce_args(func):
+        """Returns a decorator that enforces the given args exist"""
         def wrapper(*args, **kwargs):
+            """
+            Raises a MissingArgumentError if any of the required arguments
+            are missing in the given args dict
+            """
             for arg in required_args:
                 if not arg in args[1]:
                     raise errors.MissingArgumentError(
@@ -15,16 +30,20 @@ def resource_action(required_args):
         return wrapper
     return enforce_args
 
-class Resource:
+class Resource(object):
+    """
+    A Base Resource class which is the parent class of all resources that
+    we can define rules over. Defines a number of default actions.
+    """
     def __init__(self, data):
         self.data = data
 
     def __getitem__(self, key):
         return self.data[key]
 
-    def __eq__(self, A):
-        if isinstance(A, Resource):
-            return A.data == self.data
+    def __eq__(self, other):
+        if isinstance(other, Resource):
+            return other.data == self.data
         return False
 
     def __str__(self):
@@ -33,8 +52,19 @@ class Resource:
     def __repr__(self):
         return repr(self.data)
 
+    def tag(self, args):
+        """
+        Adds a tag to this resource using the given
+        `key` and `value` args
+        """
+        raise NotImplementedError()
+
     @resource_action(['after'])
     def mark_for_deletion(self, args):
+        """
+        Marks this resource for deletion after a given period of time.
+        If resources continue to match the rule, they are deleted after a time
+        """
         cal = parsedatetime.Calendar()
         time_struct, parse_status = cal.parse(args['after'])
         if parse_status == 0:
@@ -45,6 +75,9 @@ class Resource:
         self.tag({'key': 'SytheDeletionTime', 'value': str(time.timestamp())})
 
 class EC2Instance(Resource):
+    """
+    A resource for an instance in EC2.
+    """
     def __init__(self, data):
         if 'Tags' in data:
             for tag in data['Tags']:
@@ -60,6 +93,10 @@ class EC2Instance(Resource):
         )
 
 def get_ec2_instances(ec2_client=sythe.aws.get_ec2_client()):
+    """
+    Gets all the EC2 instances using the configuration from a given
+    ec2 client. Handles pagination basically.
+    """
     instances = []
     instance_page = ec2_client.describe_instances()
     instance_from_page = [instance for reservation in instance_page['Reservations']
@@ -71,6 +108,3 @@ def get_ec2_instances(ec2_client=sythe.aws.get_ec2_client()):
                               for instance in reservation['Instances']]
         instances = instances + instance_from_page
     return [EC2Instance(instance) for instance in instances]
-
-def filter_resources(resources, condition):
-    return [resource for resource in resources if condition.execute(resource)]
