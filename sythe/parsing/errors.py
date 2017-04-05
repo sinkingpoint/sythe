99f80910bed42fe148a5654cdf09ffb7ac9e9da4
Colin Douch <colin@quirl.co.nz>
2017-04-05 17:10:53 +1200

    Remove exception bodies
    Instead of having our own exception message handling, we should
    rely on the parent Exception class

--- sythe/parsing/errors.py
+++ sythe/parsing/errors.py
@@ -6,9 +6,4 @@ class ParsingError(Exception):
     A generic error raised when an assumption is broken
     when parsing a rule
     """
-    def __init__(self, message):
-        Exception.__init__(self)
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    pass
