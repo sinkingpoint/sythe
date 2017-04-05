0e3d8c7f24943a9a1385cca255325f91cdfebab7
Colin Douch <colin@quirl.co.nz>
2017-04-05 16:52:25 +1200

    Added docstrings to sythe.errors

--- sythe/errors.py
+++ sythe/errors.py
@@ -1,14 +1,23 @@
 class MissingArgumentError(Exception):
+    """
+    Raised when an expected argument for an action
+    is not provided
+    """
     def __init__(self, message):
+        Exception.__init__(self)
         self.message = message
 
     def __str__(self):
         return self.message
 
 class InvalidArgumentError(Exception):
+    """
+    Raised when the value to an argument given to an action
+    is invalid in the context of the action
+    """
     def __init__(self, message):
+        Exception.__init__(self)
         self.message = message
 
     def __str__(self):
         return self.message
-
